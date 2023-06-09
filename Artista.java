3447a3c1fca288059eabb318b9591aa0c550078b
Vanessa <130495689+eissenv@users.noreply.github.com>
2023-06-09 18:07:03 -0400

    Update Artista.java

--- Artista.java
+++ Artista.java
@@ -1,1 +1,36 @@
 
+import java.util.Date;
+
+public class Artista extends Usuario {
+	private int streamingsTotais;
+	private float valorGanho = 0;
+	private float taxa;
+	private String[] albuns;
+	
+	// Construtor de Artista
+	Artista(String nome, String cpf, Date datanasc, String telefone, String email){
+		super(nome, cpf, datanasc, telefone, email);
+	}
+	
+	 // Método para aumentar o valorGanho a cada streaming
+    public void aumentarStreamings() {
+        streamingsTotais++;
+        valorGanho += 0.025f;
+    }
+    
+	// GETTERS
+	public int getStreamingsTotais() {
+		return streamingsTotais;
+	}
+	public float getValorGanho() {
+		return valorGanho;
+	}
+	public float getTaxa() {
+		return taxa;
+	}
+	public String[] getAlbuns() {
+		return albuns;
+	}
+
+
+}
