a50c831f066040822ddfd30701a8568ad1f3d770
Vanessa <130495689+eissenv@users.noreply.github.com>
2023-06-09 22:10:23 -0400

    Update Gratuito.java

--- Gratuito.java
+++ Gratuito.java
@@ -1,1 +1,20 @@
+public class Gratuito extends Plano {
+    private boolean anuncio = true; // Precisa implementar, quando <condicao satisfeita> getAnuncio
+    private String descricao = "Você pode utilizar o aplicativo de maneira gratuita, com anúncios.";
 
+
+    public String toString() {
+        return "Plano Gratuito: " + descricao;
+    }
+    
+    public String getDescricao() {
+    	return descricao;
+    }
+    
+    public boolean getAnuncio() {
+    	return anuncio;
+    }    	
+    
+    
+    
+}
