5bfa229fe32f5000b6974c695466220f6d1e3c77
Vanessa <130495689+eissenv@users.noreply.github.com>
2023-06-09 22:24:41 -0400

    Update Pago.java

--- Pago.java
+++ Pago.java
@@ -1,11 +1,24 @@
+package Um;
+
 public class Pago extends Plano {
     private String metodoPagamento;
     private double preco = 15.99;
     private String descricao = "Através desse plano você poderá desfrutar de todas as funcionalidades do aplicativo sem anúncios.";
+    private boolean planoAtivo; // talvez passar isso para usuário
 
     Pago(String metodoPagamento){
     	this.metodoPagamento = metodoPagamento;
+    	realizarPagamento(); // ainda planejando, talvez passar todos esses métodos de pagamento para usuário
     }
+    
+    public void realizarPagamento() {
+    	if(metodoPagamento == "efetuado") {
+    		planoAtivo = true;
+    	}else {
+    		planoAtivo = false;
+    	}
+    }
+    
     
     public String toString() {
         return "Plano Pago: " + descricao + "Preço: " + preco;
