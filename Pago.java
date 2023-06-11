a4e7cf50800db8ee88e5ded7ba6b505887724f8a
Vanessa <130495689+eissenv@users.noreply.github.com>
2023-06-11 14:06:25 -0400

    Update Pago.java

--- Pago.java
+++ Pago.java
@@ -1,34 +1,42 @@
-package Um;
-
 public class Pago extends Plano {
     private String metodoPagamento;
     private double preco = 15.99;
     private String descricao = "Através desse plano você poderá desfrutar de todas as funcionalidades do aplicativo sem anúncios.";
-    private boolean planoAtivo; // talvez passar isso para usuário
-
+    private boolean planoAtivo; 
+    
     Pago(String metodoPagamento){
     	this.metodoPagamento = metodoPagamento;
-    	realizarPagamento(); // ainda planejando, talvez passar todos esses métodos de pagamento para usuário
+    	realizarPagamento(); 
     }
     
-    public void realizarPagamento() {
-    	if(metodoPagamento == "efetuado") {
-    		planoAtivo = true;
-    	}else {
-    		planoAtivo = false;
-    	}
-    }
+    public void realizarPagamento() {	
+    	if (metodoPagamento.equalsIgnoreCase("cartão de crédito") ||
+                metodoPagamento.equalsIgnoreCase("cartao de credito") ||
+                metodoPagamento.equalsIgnoreCase("cartão de debito") ||
+                metodoPagamento.equalsIgnoreCase("cartao de debito") ||
+                metodoPagamento.equalsIgnoreCase("boleto") ||
+                metodoPagamento.equalsIgnoreCase("pix")) {
+        		planoAtivo = true;
+        	} else {
+        		planoAtivo = false;
+        	}
+        }
     
     
     public String toString() {
-        return "Plano Pago: " + descricao + "Preço: " + preco;
+        return "Plano Pago: " + descricao + " Preço: " + preco;
     }
     
+    // SETTERS
     public void setMetodoPagamento(String metodoPagamento) {
         this.metodoPagamento = metodoPagamento;
     }
     
+    // GETTERS
     public String getMetodoPagamento() {
     	return metodoPagamento;
+    }
+    public boolean getPlanoAtivo() {
+    	return planoAtivo;
     }
 }
