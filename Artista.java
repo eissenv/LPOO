acb77dccd3749643cc5813de5fe1e42d4c8f8072
Vanessa <130495689+eissenv@users.noreply.github.com>
2023-06-11 13:49:30 -0400

    Update Artista.java

--- Artista.java
+++ Artista.java
@@ -1,15 +1,16 @@
-import java.util.Date;
-
+import java.util.List;
+import java.util.ArrayList;
 
 public class Artista extends Usuario {
 	private int streamingsTotais;
 	private float valorGanho = 0;
 	private float taxa;
-	private String[] albuns;
+	private List<Album> albuns;
 	
 	// Construtor de Artista
-	Artista(String nome, String cpf, Date datanasc, String telefone, String email, String tipoPlano){
-		super(nome, cpf, datanasc, telefone, email, tipoPlano);
+	Artista(String id, String nome, String cpf, String dataNascimento, String telefone, String email, String tipoPlano, String metodoPagamento){
+		super(id, nome, cpf, dataNascimento, telefone, email, tipoPlano, metodoPagamento);
+		albuns = new ArrayList<>();
 	}
 	
 	 // Método para aumentar o valorGanho a cada streaming
@@ -17,6 +18,25 @@ public class Artista extends Usuario {
         streamingsTotais++;
         valorGanho += 0.025f;
         taxa += 0.10;
+    }
+    
+ // Método para adicionar um álbum à lista de álbuns do artista
+    public void adicionarAlbum(Album album) {
+        albuns.add(album);
+    }
+    
+ // Método para exibir todos os álbuns criados pelo artista
+    public void exibirAlbuns() {
+        if (!albuns.isEmpty()) {
+            System.out.println("Álbuns de " + getNome() + ":");
+            for (Album album : albuns) {
+            	System.out.println("Nome: " + album.getNome());
+                System.out.println("Data de Lançamento: " + album.getDataLancamento());
+                System.out.println();
+            }
+        } else {
+            System.out.println("O artista " + getNome() + " ainda não criou nenhum álbum.");
+        }
     }
     
 	// GETTERS
@@ -29,9 +49,8 @@ public class Artista extends Usuario {
 	public float getTaxa() {
 		return taxa;
 	}
-	public String[] getAlbuns() {
-		return albuns;
-	}
-
+	public List<Album> getAlbuns() {
+        return albuns;
+    }
 
 }
