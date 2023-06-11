44a5c4cd7bb8e78cdbde04680c80d29db63a3c7f
luidll <136125264+luidll@users.noreply.github.com>
2023-06-10 23:36:14 -0300

    Update Musica.java

--- Musica.java
+++ Musica.java
@@ -1,17 +1,83 @@
+package dev;
+
 public class Musica {
-	String codigo;
-	String nome;
-	String genero;
-	String artista;
-	String escritor; // Escritor e produtor adicionados por meio de métodos
-	String produtor;
-	
-	// Construtor de música
-	Musica(String nome, String genero, String artista){
-		this.nome = nome;
-		this.genero = genero;
-		this.artista = artista;
-	}
+    private int codigo;
+    private String nome;
+    private String genero;
+    private String artista;
+    private String escritor;
+    private String produtor;
 
+    public Musica(int codigo, BibliotecaMusical biblioteca) {
+        this.codigo = codigo;
+        setMusicaInfo();
+        
+        biblioteca.adicionarGenero(genero);
+        biblioteca.adicionarMusica(this);
+    }
+
+    private void setMusicaInfo() {
+        switch (codigo) {
+            case 1:
+                nome = "Bohemian Rhapsody";
+                genero = "Rock";
+                artista = "Queen";
+                escritor = "Freddie Mercury";
+                produtor = "Roy Thomas Baker";
+                break;
+            case 2:
+                nome = "Imagine";
+                genero = "Pop";
+                artista = "John Lennon";
+                escritor = "John Lennon";
+                produtor = "Phil Spector";
+                break;
+            case 3:
+                nome = "Hotel California";
+                genero = "Rock";
+                artista = "Eagles";
+                escritor = "Don Henley, Glenn Frey, Don Felder";
+                produtor = "Bill Szymczyk";
+                break;
+            case 4:
+                nome = "Billie Jean";
+                genero = "Pop";
+                artista = "Michael Jackson";
+                escritor = "Michael Jackson";
+                produtor = "Quincy Jones";
+                break;
+            // Adicione mais cases conforme necessário
+            default:
+                nome = "Música Desconhecida";
+                genero = "Desconhecido";
+                artista = "Desconhecido";
+                escritor = "Desconhecido";
+                produtor = "Desconhecido";
+                break;
+        }
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public String getEscritor() {
+        return escritor;
+    }
+
+    public String getProdutor() {
+        return produtor;
+    }
 }
-
