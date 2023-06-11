2c41d6f1d8a64f1fba5612f875e06038fc13d032
Vanessa <130495689+eissenv@users.noreply.github.com>
2023-06-11 19:54:08 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -83,5 +83,89 @@ public class Plano {
 	}
 
 
+	
+        
+   
+/*System.out.println("Álbuns favoritos de Maria:");
+             for (Album album : maria.getAlbunsFavoritos()) {
+                 System.out.println("- " + album.getNome());
+             }*/
+
+
+
+
+/*		// Testes de criação de usuário
+		System.out.println("Criando um usuário com plano pago:");
+		Usuario maria = new Usuario("1", "Maria", "85674359845", "03/01/2000", "4685659741", "maria@gmail.com", "Pago", "PIX");
+		
+		System.out.println("Criando um usuário com plano Gratuito:");
+        Usuario jorge = new Usuario("2", "Jorge", "78965412365", "15/05/1995", "987654321", "jorge@gmail.com", "Gratuito", "Nenhum");
+        
+        System.out.println("Criando um usuário com plano pago, mas a forma de pagamento não é válida:");
+        Usuario caio = new Usuario("3", "Caio", "98765432198", "28/07/1998", "123456789", "caio@gmail.com", "Pago", "Fiado");
+ 
+        System.out.println("Criando um usuário do tipo artista: ");
+        Artista rihanna = new Artista("4", "Rihanna", "56386940390", "22/06/1990", "8956372346", "rihanna@gmail.com", "Pago", "PIX");
+
+        
+        
+        
+        // Testes de criação de álbum
+      
+        Album album = new Album("3", "Novo Álbum", rihanna, "10/05/2021");
+        rihanna.adicionarAlbum(album);
+        
+        Album album2 = new Album("4", "Novo Álbum2", rihanna, "07/01/2023");
+        rihanna.adicionarAlbum(album2);
+        
+        rihanna.exibirAlbuns();
+        
+        
+        // Teste para calcular a idade:
+        System.out.println("A idade de " + maria.getNome() + " é: " + maria.calcularIdade() + " anos."); 
+
+		// Testes para adicionar músicas, álbuns e artistas aos favoritos do usuário:
+		maria.exibirAlbunsFavoritos();
+		maria.exibirArtistasFavoritos();
+		maria.exibirMusicasFavoritas();
+
+
+*/
+
+
+
+/*
+
+public class Plano {
+	   private String nome;
+	   private String descricao;
+
+	    public Plano(String nome, String descricao) {
+	        this.nome = nome;
+	        setDescricao(descricao);
+	    }
+	    
+	    public void setDescricao(String descricao) {
+	        if (descricao.length() <= 240) {
+	            this.descricao = descricao;
+	        } else {
+	            throw new IllegalArgumentException("A descrição deve ter no máximo 240 caracteres.");
+	        }
+	    }
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public String getDescricao() {
+	        return descricao;
+	    }
+
+	    
+	}
+
+
+
+*/
 
 */
